16c03130a0343947bb4ba716635346c28ee644b9
mathias314 <mathew.clutter@mines.sdsmt.edu>
2022-11-04 09:17:17 -0600

    adding starting files !!!!

---
+++ bisectingQuadrilaterals.cpp
@@ -0,0 +1,13 @@
+// Mathew Clutter
+// Adam Kraus
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    cout << "Epic program to find x intercept of line that cuts quad in half by volume!!!!" << endl;
+}
