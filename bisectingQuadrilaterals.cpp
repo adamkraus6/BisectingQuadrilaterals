cbbff9b638124724590f81ea9b9d7494e109d2c9
Kraus, Adam <adamkraus6@gmail.com>
2022-11-04 10:31:06 -0600

    input/output files

--- bisectingQuadrilaterals.cpp
+++ bisectingQuadrilaterals.cpp
@@ -4,10 +4,62 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <fstream>
 
 using namespace std;
 
+double bisect();
+
+struct Point
+{
+    double x, y;
+} points[4];
+
 int main(int argc, char **argv)
 {
-    cout << "Epic program to find x intercept of line that cuts quad in half by volume!!!!" << endl;
+    ifstream fin("quad.in");
+    ofstream fout("quad.out");
+    int caseNum = 1;
+
+    if(!fin)
+    {
+        cout << "Bad quad.in" << endl;
+        fin.close();
+        return -1;
+    }
+
+    if(!fout)
+    {
+        cout << "Bad quad.out" << endl;
+        fout.close();
+        return -1;
+    }
+
+    while(true)
+    {
+        fin >> points[0].x >> points[0].y;
+
+        if(points[0].x == -1 && points[0].y == -1)
+        {
+            break;
+        }
+
+        for(int i = 1; i < 4; i++)
+        {
+            fin >> points[i].x >> points[i].y;
+        }
+
+        double bi = bisect();
+        fout << "Case " << caseNum << ": " << bi << endl;
+        cout << "Case " << caseNum << ": " << bi << endl;
+        caseNum++;
+    }
+
+    fin.close();
+    fout.close();
+}
+
+double bisect()
+{
+    return 0;
 }
